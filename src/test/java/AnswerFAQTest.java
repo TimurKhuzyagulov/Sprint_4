c5217e2d98f73d5459514ed4c7e9146da38075a2
Timur Khuzyagulov <dekabristov41@yandex.ru>
2022-11-24 12:13:08 +0500

    commit all PROJECT

--- src/test/java/AnswerFAQTest.java
+++ src/test/java/AnswerFAQTest.java
@@ -4,7 +4,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 import static org.junit.Assert.assertTrue;
 
-//тест для важных вопросов
+//тест для важных вопросов.
 public class AnswerFAQTest {
 
     MainPage mainPage = new MainPage(MainPage.driver);
--- src/test/java/testFlow.java
+++ src/test/java/testFlow.java
@@ -8,7 +8,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
-//положительный тест
+//положительный тест.
 @RunWith(Parameterized.class)
 public class testFlow {
 
